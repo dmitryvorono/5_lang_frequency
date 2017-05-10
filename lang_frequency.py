35606ecffb8a60673a55afa93fadd2621449c5bb
dvoronov <voronov-d88@yandex.ru>
2017-05-10 08:10:28 +0300

    Refactoring

--- lang_frequency.py
+++ lang_frequency.py
@@ -2,12 +2,13 @@ import sys
 import os
 import re
 import collections
+import pprint
 
 
 def load_data(filepath):
     words_counter = collections.Counter()
     if not os.path.exists(filepath):
-        return None
+        sys.exit('File does not exists: {0}'.format(filepath))
     for line in open(filepath, 'r'):
         words = [word.lower() for word in re.split('\W+', line) if word]
         words_counter.update(words)
@@ -18,11 +19,14 @@ def get_most_frequent_words(words_counter):
     return [x[0] for x in words_counter.most_common(10)]
 
 
+def print_freq_words(word_list):
+    pprint.pprint(word_list)
+
+
 if __name__ == '__main__':
     try:
         file_name = sys.argv[1]
     except IndexError:
-        print('Usage: python lang_frequency.py <path to file>')
-        sys.exit(2)
+        sys.exit('Usage: python lang_frequency.py <path to file>')
     words_counter = load_data(file_name)
-    print(get_most_frequent_words(words_counter))
+    print_freq_words(get_most_frequent_words(words_counter))
