2a4b64eda4e7ac42eb5b55200cc8edda3a7a4b42
dvoronov <voronov-d88@yandex.ru>
2017-05-12 07:51:14 +0300

    Refactoring load_data

--- lang_frequency.py
+++ lang_frequency.py
@@ -6,10 +6,14 @@ import pprint
 
 
 def load_data(filepath):
-    words_counter = collections.Counter()
     if not os.path.exists(filepath):
         sys.exit('File does not exists: {0}'.format(filepath))
-    for line in open(filepath, 'r'):
+    return open(filepath, 'r')
+
+
+def make_words_counter(file_handler):
+    words_counter = collections.Counter()
+    for line in file_handler:
         words = [word.lower() for word in re.split('\W+', line) if word]
         words_counter.update(words)
     return words_counter
@@ -24,8 +28,10 @@ def print_freq_words(word_list):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    count_required_sys_args = 2
+    count_freq_words = 10
+    if len(sys.argv) != count_required_sys_args:
         sys.exit('Usage: python lang_frequency.py <path to file>')
-    file_name = sys.argv[1]
-    words_counter = load_data(file_name)
-    print_freq_words(get_most_frequent_words(words_counter, 10))
+    file_handler = load_data(sys.argv[1])
+    words_counter = make_words_counter(file_handler)
+    print_freq_words(get_most_frequent_words(words_counter, count_freq_words))
